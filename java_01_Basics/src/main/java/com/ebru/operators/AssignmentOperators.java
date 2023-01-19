e3b73991934e84b75b2a746dcf6be9ad58549746
ebru-celik <ebrucelikcinar@gmail.com>
2023-01-19 12:54:42 +0100

    Operators
    update : assignment with initialization

--- java_01_Basics/src/main/java/com/ebru/operators/AssignmentOperators.java
+++ java_01_Basics/src/main/java/com/ebru/operators/AssignmentOperators.java
@@ -46,6 +46,35 @@ public class AssignmentOperators {
         v1 &= v2; // v1 = (byte) (v1 & v2)
         System.out.println("v1: " + v1);
 
-    }
+        /*
+         * boolean is only compatible with boolean!
+         * int a = true; // compiler error!
+         * boolean b = 22; // compiler error!
+         */
 
+        /*
+         * byte -> short -> int -> long -> float -> double
+         * The order helps identify when an assignment compiles without casting.
+         */
+
+        long l1 = Long.MAX_VALUE;
+        System.out.println("l1: " + l1); // 9_223_372_036_854_775_807
+
+        float f1 = l1; // float <- long  (it works)
+        System.out.println("f1: " + f1); //  9.223372E18
+
+		//l1 = f1; // compiler error: long <- float
+
+        byte b3 = 12; // implicit casting bei: byte <- int (with Literal)
+        /*
+         * char
+         */
+        char ch = 97;  // implicit casting: char ch = (char) 97;
+
+//		byte v3 = ch;  // compiler error! byte <- char
+
+//		short v4 = ch; // compiler error! short <- char
+
+        int v5 = ch;	// int <- char
+    }
 }
